f247c86845d7aecba6963adcfceb48ef9da6cdc7
hyeoneedyou <hyeoneedyou@naver.com>
2021-08-02 03:00:45 +0900

    feat: Add goal main template

--- main/urls.py
+++ main/urls.py
@@ -5,5 +5,6 @@ app_name = 'main'
 urlpatterns = [
     path('', views.main_logout, name="main_logout"),
     path('login/', views.main_login, name="main_login"),
+    path('goal_main/', views.goal_main, name="goal_main"),
     path('add_goal/', views.add_goal, name="add_goal"),
 ]
--- main/views.py
+++ main/views.py
@@ -9,5 +9,9 @@ def main_logout(request):
     return render(request, 'main/main_logout.html')
 
 
+def goal_main(request):
+    return render(request, 'main/goal_main.html')
+
+
 def add_goal(request):
     return render(request, 'main/add_goal.html')
