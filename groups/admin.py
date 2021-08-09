b1cd4d6c24d3515ed80bdb0517bc470e77af6729
hyeoneedyou <hyeoneedyou@naver.com>
2021-08-10 01:41:30 +0900

    feat: Add group page - personal, show certify

--- groups/admin.py
+++ groups/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
-from .models import CertifyImage, CertifyText, CertifyFigure
+from .models import Certify
 
-admin.site.register(CertifyImage)
-admin.site.register(CertifyText)
-admin.site.register(CertifyFigure)
+admin.site.register(Certify)
