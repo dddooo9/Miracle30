4ea15199956c02444fc898a86b3c63ce9191e774
SJLEE316 <lsj8249@naver.com>
2021-08-19 17:23:22 +0900

    .

--- groups/urls.py
+++ groups/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('<int:goal_id>/certify/', views.certify, name="certify"),
     path('<int:goal_id>/personal/', views.personal, name="personal"),
     path('<int:goal_id>/show_certify/', views.show_certify, name="show_certify"),
-    path('select/', views.select, name="select"),
+    path('group_list/', views.group_list, name="group_list"),
+    path('group_detail/', views.group_detail, name="group_detail"),
     path('make_group/', views.make_group, name="make_group")
 ]
