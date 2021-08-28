477ffec96bcc26c78eaf079e98b3c158975db057
SJLEE316 <lsj8249@naver.com>
2021-08-28 13:29:11 +0900

    modal

--- Miracle30/static/js/group_main.js
+++ Miracle30/static/js/group_main.js
@@ -1,10 +1,33 @@
-const btn = document.querySelectorAll(".btn"),
-pTag = document.getElementsByTagName('p'),
-modal = document.querySelector('.modal-body');
+// modal 창 띄우기
+const modal = document.getElementById("myModal"),
+  modalBtn = document.querySelectorAll(".btn"), // 버튼 누르면 모달버튼 뜸
+  closeBtn = document.querySelector(".close");
 
-btn.forEach((elem, index) => {
-    elem.addEventListener("click", (e) => {
-        var data = pTag[index + 1].textContent;
-        modal.textContent = data;
-    });
+// modalBtn.onclick = function () {
+//   modal.style.display = "block";
+// };
+
+closeBtn.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+// 데이터 넣기
+// const btn = document.querySelectorAll(".btn"),
+const pTag = document.getElementsByTagName("p"),
+  modalBody = document.querySelector(".modal-body");
+
+modalBtn.forEach((elem, index) => {
+  elem.addEventListener("click", (e) => {
+    modal.style.display = "block";
+    let data = pTag[index - 2].textContent;
+    data = data.replace(/'/gi, "");
+    data = data.substr(1, data.length - 2);
+    modalBody.textContent = data;
+  });
 });
